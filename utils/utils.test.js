d6f2f7415c5799c88839198fe366695df7801234
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-04-24 17:16:10 +0100

    added mocha

---
+++ utils/utils.test.js
@@ -0,0 +1,21 @@
+const utils = require('./utils');
+
+it('should add two numbers', ()=>{
+    var res = utils.add(33, 11);
+    
+    if (res !== 44) {
+        throw new Error(`Expected 44, but you got ${res}`)
+    }else{
+        console.log('Well done, good job buddy!');
+    }
+});
+
+it('should square two numbers', ()=>{
+    var res = utils.square(2);
+
+    if (res !== 4){
+        throw new Error(`Expected 4, but got ${res}`)
+    }else{
+        console.log('Wow, ur so good a mathfs');
+    }
+})
