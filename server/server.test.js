f77d7bc47cb2be42a40c5bf5bc5f05b80f33a6a2
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-04-25 15:52:38 +0100

    updated .test files to use describe()

--- server/server.test.js
+++ server/server.test.js
@@ -3,27 +3,37 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('should return hello world as a response', (done)=>{
-    request(app)
-    .get('/')
-    .expect(404)
-    .expect((res)=>{
-        expect(res.body).toInclude({
-            error: 'Page not found'
+describe('GET', ()=>{
+
+    it('should return hello world as a response', (done)=>{
+        request(app)
+        .get('/')
+        .expect(404)
+        .expect((res)=>{
+            expect(res.body).toInclude({
+                error: 'Page not found'
+            });
+        })
+        .end(done)
+    });
+    
+
+    describe('GET / #users', ()=>{
+        it('should return the user object', (done)=>{
+            request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body).toInclude({
+                    name: 'Cosmo',
+                    age: 28
+                });
+            })
+            .end(done)
         });
-    })
-    .end(done)
+    });
+    
+
+
 });
 
-it('should return the user object', (done)=>{
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res)=>{
-        expect(res.body).toInclude({
-            name: 'Cosmo',
-            age: 28
-        });
-    })
-    .end(done)
-});
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,58 +2,63 @@ const utils = require('./utils');
 
 const expect = require('expect');
 
-it('should add two numbers', ()=>{
-    var res = utils.add(33, 11);
+describe('Utils', ()=>{
 
-    expect(res).toBe(44).toBeA('number');
+    it('should add two numbers', ()=>{
+        var res = utils.add(33, 11);
     
-    /* if (res !== 44) {
-        throw new Error(`Expected 44, but you got ${res}`)
-    }else{
-        console.log('Well done, good job buddy!');
-    } */
-});
-
-it('should async add two numbers', (done)=>{
-    utils.asyncAdd(4, 3, (sum)=>{
-        expect(sum).toBe(7).toBeA('number');
-        done();
+        expect(res).toBe(44).toBeA('number');
+        
+        /* if (res !== 44) {
+            throw new Error(`Expected 44, but you got ${res}`)
+        }else{
+            console.log('Well done, good job buddy!');
+        } */
     });
-});
-
-it('should square two numbers', ()=>{
-    var res = utils.square(2);
-
-    expect(res).toBe(4).toBeA('number');
-
-/*     if (res !== 4){
-        throw new Error(`Expected 4, but got ${res}`)
-    }else{
-        console.log('Wow, ur so good a mathfs');
-    } */
-});
-
-it('should async square two numbers', (done)=>{
-    utils.asyncSquare(4, (square)=>{
-        expect(square).toBe(16).toBeA('number');
-        done();
-    });
-});
-
-it('should cube the number', ()=>{
-    var res = utils.cube(3);
-
-    if (res !== 27){
-        throw new Error(`Expected 27, but got ${res}`);
-    }
-});
-
-/* it('should verify first and last names are set', ()=>{
-    var user = {location: 'London', age: 28};
-    var res = utils.setName(user, 'Cosmo Swarbreck');
     
-    expect(res).toInclude({
-        firstName: 'Cosmo',
-        lastName: 'Swarbreck'
+    it('should async add two numbers', (done)=>{
+        utils.asyncAdd(4, 3, (sum)=>{
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        });
     });
-}); */
+    
+    it('should square two numbers', ()=>{
+        var res = utils.square(2);
+    
+        expect(res).toBe(4).toBeA('number');
+    
+    /*     if (res !== 4){
+            throw new Error(`Expected 4, but got ${res}`)
+        }else{
+            console.log('Wow, ur so good a mathfs');
+        } */
+    });
+    
+    it('should async square two numbers', (done)=>{
+        utils.asyncSquare(4, (square)=>{
+            expect(square).toBe(16).toBeA('number');
+            done();
+        });
+    });
+    
+    it('should cube the number', ()=>{
+        var res = utils.cube(3);
+    
+        if (res !== 27){
+            throw new Error(`Expected 27, but got ${res}`);
+        }
+    });
+    
+    /* it('should verify first and last names are set', ()=>{
+        var user = {location: 'London', age: 28};
+        var res = utils.setName(user, 'Cosmo Swarbreck');
+        
+        expect(res).toInclude({
+            firstName: 'Cosmo',
+            lastName: 'Swarbreck'
+        });
+    }); */
+
+});
+
